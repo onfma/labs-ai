c5946feb05c7632e5dd0f52f126e4f452dad7e37
iustina0 <iustinaapostol02@gmail.com>
2023-11-17 09:24:33 +0200

    fix

--- tema4/backpropag.py
+++ tema4/backpropag.py
@@ -19,13 +19,12 @@ input_size = x_train.shape[1]
 hidden_layer = 64
 output_layer = 3
 learning_rate = 0.001
-nr_epochs = 10
+nr_epochs = 1000
 
 #Initialize weights
 np.random.seed(42)
 weights_hidden = np.random.randn(input_size, hidden_layer)
 weights_output = np.random.randn(hidden_layer, output_layer) 
-print(len(x_test))
 
 # Activation functions and derivatives
 def sigmoid(x):
@@ -33,6 +32,10 @@ def sigmoid(x):
 
 def sigmoid_derivative(x):
     return sigmoid(x) * (1 - sigmoid(x))
+
+def softmax(x):
+    exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
 
 # Error function and its derivative
 def mean_squared_error(predicted, target):
@@ -42,38 +45,33 @@ def mean_squared_error_derivative(predicted, target):
     return 2 * (predicted - target.reshape(-1, 1)) / len(target)
 
 # Forward propagation
-def forward_propagation(x_samples, weights_hidden, weights_output):
-    outputs = []
-    hidden = []
-    for x_sample in x_samples:
-        hidden_activation = sigmoid(np.dot(x_sample, weights_hidden))
-        output_activation = sigmoid(np.dot(hidden_activation, weights_output))
-        hidden.append(hidden_activation)
-        outputs.append(output_activation)
-    return outputs, hidden
-
+def forward_propagation(x, weights_hidden, weights_output):
+    hidden_activation = sigmoid(np.dot(x, weights_hidden))
+    output_activation = sigmoid(np.dot(hidden_activation, weights_output))
+    return hidden_activation, output_activation
 
 sample_indices = np.random.choice(len(x_train), size=5, replace=False)
 x_sample = x_train[sample_indices]
 
-outputs, hidden = forward_propagation(x_sample, weights_hidden, weights_output)
+# Forward propagation
+hidden_activation, output_activation = forward_propagation(x_sample, weights_hidden, weights_output)
 
-print("Input layer")
+# Print the results
+print("Input:")
 print(x_sample)
-# print("Output of the neurons for hidden layer:")
-# print(np.array(hidden))
-print("Output of the neurons for output layer")
-print(np.array(outputs))
-
+#print("\nHidden Layer Activation:")
+#print(hidden_activation)
+print("\nOutput Layer Activation:")
+print(output_activation)
 # Back propagation
-def back_propagation(x_samples, y_samples, weights_hidden, weights_output, learning_rate):
-    outputs, hidden = forward_propagation(x_samples, weights_hidden, weights_output)
+def back_propagation(x, y, weights_hidden, weights_output, learning_rate):
+    hidden, outputs = forward_propagation(x, weights_hidden, weights_output)
 
-    output_errors = mean_squared_error_derivative(np.array(outputs), np.array(y_samples)) # eroare output layer
+    output_errors = mean_squared_error_derivative(np.array(outputs), np.array(y)) # eroare output layer
     hidden_errors = np.dot(output_errors, weights_output.T) * sigmoid_derivative(np.array(hidden)) # eroare hidden layer
 
-    weights_output -= learning_rate * np.dot(np.array(hidden).T, output_errors) / len(x_samples) # update weights output layer
-    weights_hidden -= learning_rate * np.dot(np.array(x_samples).T, hidden_errors) / len(x_samples) # update weights hidden layer
+    weights_output -= learning_rate * np.dot(np.array(hidden).T, output_errors) / len(x) # update weights output layer
+    weights_hidden -= learning_rate * np.dot(np.array(x).T, hidden_errors) / len(x) # update weights hidden layer
 
     return weights_hidden, weights_output
 
@@ -82,12 +80,12 @@ for epoch in range(nr_epochs):
     weights_hidden, weights_output = back_propagation(x_train, y_train, weights_hidden, weights_output, learning_rate)
 
     # squared error on the training set
-    train_outputs, _ = forward_propagation(x_train, weights_hidden, weights_output)
+    _, train_outputs = forward_propagation(x_train, weights_hidden, weights_output)
     train_error = mean_squared_error(train_outputs, y_train)
     #print(f"Epoch {epoch + 1}/{nr_epochs}, Train Error: {train_error}")
 
 # testare retea antrenata
-test_outputs, _ = forward_propagation(x_test, weights_hidden, weights_output)
+_, test_outputs = forward_propagation(x_test, weights_hidden, weights_output)
 
 test_error = mean_squared_error(test_outputs, y_test)
 print(f"Test Error: {test_error}") # 1.58
@@ -95,8 +93,14 @@ print(f"Test Error: {test_error}") # 1.58
 def accuracy_score(y_test, predictions):
     correct_predictions = np.sum(y_test == predictions)
     return correct_predictions / len(y_test)
-predictions = np.argmax(test_outputs, axis=1) 
+predictions = np.argmax(test_outputs, axis=1) + 1
+
 print(predictions)
 print(y_test)
+
+print("for 5th line:")
+print(f"Atributres: {x_test[4]} \t Class: {y_test[4]}")
+print(f"Output layer weights : {test_outputs[4]}")
+print(f"Network prediciton: {predictions[4]}")
 accuracy = accuracy_score(y_test, predictions)
 print(f"Accuracy on the test set: {accuracy}") # 0.38
